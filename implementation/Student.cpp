73d09eeec75b16cfd66d282646e3462ad3bffcd2
lejie <linlejie1998@gmail.com>
2022-01-10 22:52:33 +0800

    1

---
+++ implementation/Student.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "Student.h"
+using namespace std;
+
+void Student::FirstName(void) {
+    return "Lejie";
+}
+
+void Student::LastName(void) {
+    return "Lin";
+}
+
+void Student::StudentIdentifier(int) {
+    return 2069983;
+}
+
