d5bbd8191424df5f331bdd8ae35f3f14ff88f2f4
lejie <linlejie1998@gmail.com>
2022-01-10 22:39:52 +0800

    1

---
+++ implementation/main.cpp
@@ -0,0 +1,12 @@
+#include "Student.h"
+#include <iostream>
+using namespace std;
+
+main() {
+	Student stu;
+
+
+	cout << stu.FirstName() << stu.LastName() << stu.StudentIdentifier();
+
+	return 0;
+}
