5e551e84026d9a9c4b093b76d37b0b935dc62032
lejie <linlejie1998@gmail.com>
2022-01-10 21:56:17 +0800

    ass1

---
+++ implementation/Student.h
@@ -0,0 +1,18 @@
+#include <string>
+using namespace std;
+
+class Student {
+public:
+    Student() {}
+    string FirstName() {
+        return "Lejie";
+    }
+
+    string LastName() {
+        return "Lin";
+    }
+
+    int StudentIdentifier() {
+        return 2069983;
+    }
+};
